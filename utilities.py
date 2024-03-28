28bca8d9c436ac8847217bd19247319ad6c1ca08
Samuel Brookes <sghb2@cam.ac.uk>
2024-03-28 18:08:38 +0000

    Revert parallelising the loading of positions.

--- utilities.py
+++ utilities.py
@@ -62,7 +62,7 @@ class AtomPos:
         print('Parsing through frames.')
         print(f'Total: {length}.')
         
-        def process_frame(ts):
+        for ts in self._u.trajectory[self._start:self._end]:
             oh_dist = distance_array(self._u.select_atoms('name' + ' OW').positions, # distance array loaded from module
                                     self._u.select_atoms('name' + ' H').positions, 
                                     box=self._u.dimensions)
@@ -91,9 +91,41 @@ class AtomPos:
                 cpos_traj.append(cpos)
                 ocpos1_traj.append(ocpos)
             box_dim.append(self._u.dimensions)
-        num_cores = int(multiprocessing.cpu_count()/2)
-        result = Parallel(n_jobs=num_cores,backend='threading')(delayed(process_frame)(ts) for ts in self._u.trajectory[self._start:self._end])
+
         return (opos_traj, h1_traj, h2_traj, cpos_traj, ocpos1_traj, ocpos2_traj, box_dim)
+    
+        # def process_frame(ts):
+        #     oh_dist = distance_array(self._u.select_atoms('name' + ' OW').positions, # distance array loaded from module
+        #                             self._u.select_atoms('name' + ' H').positions, 
+        #                             box=self._u.dimensions)
+        #     idx = np.argpartition(oh_dist, 3, axis=-1)
+        #     opos = self._u.select_atoms('name' + ' OW').positions
+        #     h1pos = self._u.select_atoms('name' + ' H')[idx[:, 0]].positions
+        #     h2pos = self._u.select_atoms('name' + ' H')[idx[:, 1]].positions
+        #     opos_traj.append(opos)
+        #     h1_traj.append(h1pos)
+        #     h2_traj.append(h2pos)
+        #     c_oc_dist = distance_array(self._u.select_atoms('name' + ' C').positions, # distance array loaded from module
+        #                             self._u.select_atoms('name' + ' OC').positions, 
+        #                             box=self._u.dimensions)
+
+        #     try: 
+        #         idx = np.argpartition(c_oc_dist, 3, axis=-1)
+        #         cpos = self._u.select_atoms('name' + ' C').positions
+        #         oc1pos = self._u.select_atoms('name' + ' OC')[idx[:, 0]].positions
+        #         oc2pos = self._u.select_atoms('name' + ' OC')[idx[:, 1]].positions
+        #         cpos_traj.append(cpos)
+        #         ocpos1_traj.append(oc1pos)
+        #         ocpos2_traj.append(oc2pos)
+        #     except:  # allow exception for single co2 molecule (partitioning breaks for this.)
+        #         cpos = self._u.select_atoms('name' + ' C').positions
+        #         ocpos = self._u.select_atoms('name' + ' OC').positions
+        #         cpos_traj.append(cpos)
+        #         ocpos1_traj.append(ocpos)
+        #     box_dim.append(self._u.dimensions)
+        # num_cores = int(multiprocessing.cpu_count()/2)
+        # result = Parallel(n_jobs=num_cores,backend='threading')(delayed(process_frame)(ts) for ts in self._u.trajectory[self._start:self._end])
+        # return (opos_traj, h1_traj, h2_traj, cpos_traj, ocpos1_traj, ocpos2_traj, box_dim)
 
 
     def positions_unorg(self):
